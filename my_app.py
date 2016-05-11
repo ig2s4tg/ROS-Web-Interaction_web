0a713d1305c0352aa3ffdf9426c78d243f0750ad
Walter Sagehorn <w.sagehorn@gmail.com>
2016-05-10 22:28:11 -0500

    halfway through website

--- my_app.py
+++ my_app.py
@@ -2,27 +2,34 @@ from flask import Flask, render_template, session, redirect, request, url_for
 from flask.ext.mobility import Mobility
 from flask.ext.mobility.decorators import mobile_template
 
-from forms import NameForm
+import urllib2
+import json
 
 app = Flask(__name__)
 Mobility(app)
 
 app.secret_key = 'CHANGE_ME'
 
+ip_url = "http://nixons-head.csres.utexas.edu:7979/hostsjson"
+req = urllib2.Request(ip_url)
+response = urllib2.urlopen(req)
+result = response.read()
+robot_ips = json.loads(result)
+
 ### ROUTING ###
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
 @mobile_template('{mobile/}index.html')
 def index(template):
-	form = NameForm(request.form)
-	if request.method == 'POST' and form.validate():
-		session["name"] = form.name.data
-	return render_template(template, form=form)
+	return render_template(template, robot_ips=robot_ips)
 
-#clears the session and then redirects back to index
-@app.route('/logout')
-def logout():
-	session["name"] = ""
-	return redirect(url_for('index'))
+@app.route('/robot/<robot_name>')
+@mobile_template('{mobile/}robot.html')
+def robot(template, robot_name):
+	#no switch in python :'(
+	if robot_name.lower() in robot_ips.keys():
+		return render_template(template, robot_name=robot_name, robot_ip=robot_ips[robot_name.lower()])
+	else:
+		return "no robot named " + robot_name
 
 
 if __name__ == "__main__":
--- start_server.py
+++ start_server.py
@@ -8,5 +8,5 @@ from tornado.ioloop import IOLoop
 from my_app import app
 
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(80)
+http_server.listen(8080)
 IOLoop.instance().start()
